bb9daa96c770de438a4c547ba65a6a5b19c2c386
Amanda Nikrant <ajnikrant@gmail.com>
2021-02-03 17:04:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,14 +8,14 @@ def game_hash()
       :team_name => "Brooklyn Nets",
       colors: ["black", "white"],
       players: [
-        :player_name=>["Alan Anderson"],
+        {:player_name=>["Alan Anderson"],
         :number=>[0],
         :shoe=>[16],
         :points=>[22],
         :rebounds=>[12],
         :assists=>[12],
         :steals=>[3],
-        :blocks=>[1],
+        :blocks=>[1]},
       ]
     },
     :away => {
