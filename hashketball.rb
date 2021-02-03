c147a4358ea8dcf53e83691334c8ef2c7d02af9c
Amanda Nikrant <ajnikrant@gmail.com>
2021-02-03 16:54:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,7 +8,7 @@ def game_hash()
       :team_name => "",
       colors: [],
       players: [
-        :player_name=>[],
+        :player_name=>[alan_anderson, reggie_evans, brook_lopez, mason_plumlee, jason_terry],
         :number=>[],
         :shoe=>[],
         :points=>[],
