93c858327ca8173e3b1e6c64cae67ad1f7bb00b3
Amanda Nikrant <ajnikrant@gmail.com>
2021-02-03 15:41:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,7 +8,7 @@ def game_hash()
       :team_name => "",
       :colors => [],
       :players => [
-        :player_name=>[].case,
+        :player_name=>[],
         :number=>[],
         :shoe=>[],
         :points=>[],
